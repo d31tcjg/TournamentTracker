423d6148da89f77fc689e3e9215de2bc4c9d355f
DELTA31TECH <d31tcjg@gmail.com>
2022-06-03 13:23:52 -0500

    Added Validation Logic to Score Entry

--- TrackerUI/TournamentViewerForm.cs
+++ TrackerUI/TournamentViewerForm.cs
@@ -158,8 +158,47 @@ namespace TrackerUI
             LoadMatchups((int)roundDropDown.SelectedItem);
         }
 
+        private string ValidateData()
+        {
+            string output = string.Empty;
+
+            double teamOneScore = 0;
+            double teamTwoScore = 0;
+
+            bool scoreOneValid = double.TryParse(teamOneScoreValue.Text, out teamOneScore);
+            bool scoreTwoValid = double.TryParse(teamTwoScoreValue.Text, out teamTwoScore);
+
+            if (!scoreOneValid)
+            {
+                output = "The Score One value is not a valid number";
+            }
+            else if (!scoreTwoValid)
+            {
+                output = "The Score Two value is not a valid number";
+            }
+
+            else if (teamOneScore == 0 && teamTwoScore == 0)
+            {
+                output = "You did not enter a score for either team";
+            }
+
+            else if (teamOneScore == teamTwoScore)
+            {
+                output = "Ww do not allow ties in this application";
+            }
+
+            return output;
+        }
+
         private void scoreButton_Click(object sender, EventArgs e)
         {
+            string errorMessage = ValidateData();
+            if (errorMessage.Length > 0)
+            {
+                MessageBox.Show($"Input Error: {errorMessage}");
+                return;
+            }
+
             var m = (MatchupModel)matchupListbox.SelectedItem;
             double teamOneScore = 0;
             double teamTwoScore = 0;
@@ -202,10 +241,19 @@ namespace TrackerUI
                 }
             }
 
-            TournamentLogic.UpdateTournamentResults(tournament);
+            try
+            {
+                TournamentLogic.UpdateTournamentResults(tournament);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"The application had the following error: {ex.Message}");
+                return;
+            }
 
             LoadMatchups((int)roundDropDown.SelectedItem);
 
         }
+
     }
 }
