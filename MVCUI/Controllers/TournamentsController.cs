f47ae16ab8ecded2fb23b99a2a124bfb1acc4c50
DELTA31TECH <d31tcjg@gmail.com>
2022-06-06 06:34:03 -0500

    Added Create Tournament Functionality
    Added custom Tournament MVC Model and DataAnnotations. Created "Create" Page for tournaments controller. Added Logic to Convert TournamentMVCModel to TournamentModel and called logix to create rounds and alert users to new rounds as was done in the DesktopUI

---
+++ MVCUI/Controllers/TournamentsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCUI.Models;
+using TrackerLibrary;
+using TrackerLibrary.Models;
+
+namespace MVCUI.Controllers
+{
+    public class TournamentsController : Controller
+    {
+        // GET: Tournaments
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: People/Create
+        public ActionResult Create()
+        {
+            TournamentMVCModel input = new TournamentMVCModel();
+            List<TeamModel> allTeams = GlobalConfig.Connection.GetTeams_All();
+            List<PrizeModel> allPrizes = GlobalConfig.Connection.GetPrizes_All();
+
+            input.EnteredTeams = allTeams.Select(x => new SelectListItem{ Text = x.TeamName, Value = x.Id.ToString() }).ToList();
+            input.Prizes = allPrizes.Select(x => new SelectListItem { Text = x.PlaceName, Value = x.Id.ToString() }).ToList();
+
+            return View(input);
+        }
+
+        // POST: People/Create
+        [ValidateAntiForgeryToken()]
+        [HttpPost]
+        public ActionResult Create(TournamentMVCModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid && model.SelectedEnteredTeams.Count > 0)
+                {
+                    TournamentModel t = new TournamentModel()
+                    {
+                        TournamentName = model.TournamentName,
+                        EntryFee = model.EntryFee,
+                        EnteredTeams = model.SelectedEnteredTeams.Select(x => new TeamModel{ Id = int.Parse(x)}).ToList(),
+                        Prizes = model.SelectedPrizes.Select(x => new PrizeModel{ Id = int.Parse(x)}).ToList()
+                    };
+
+                    // Wire our matchups
+                    TournamentLogic.CreateRounds(t);
+
+                    GlobalConfig.Connection.CreateTournament(t);
+
+                    t.AlertUsersToNewRound();
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return RedirectToAction("Create");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
